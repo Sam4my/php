d84caa0e77503437e30f05d9116330103ad8b916
Sam4my <sadako.oliveira.2003@gmail.com>
2022-09-13 23:25:55 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+$funcionario = 'salario';
+// $num = [10 , 22 , 92 , 199 , 81, 2999, 493, 1];
+
+//EX 1) Crie um programa em PHP, 
+//que calcule a soma de todos os números presentes em um array de tamanho aleatório e apresente na tela.
+
+ //echo "A soma será: ".array_sum($num)."\n";
+ 
+ 
+ //EX 2) Crie um programa que apresente o maior, 
+ //o menor e a média de todos os números presentes em um array de tamanho aleatório.
+
+//  echo "Menor número: ". min($num)."<br>";
+
+//  echo "Maior número: ". max($num)."<br>";S
+
+//  echo "média igual à: " .array_sum($num)/ count($num);
+
+ $funcionarios =array
+ (
+     ['nome' => 'João da Silva', 
+     'genero' => 'M', 
+     'area' => 'engenharia', 
+     'salario' => 6000
+    ],
+     ['nome' => 'Maria da Silva', 
+     'genero' => 'F', 
+     'area' => 'marketing', 
+     'salario' => 3000
+    ],
+     ['nome' => 'Beltrano da Silva', 
+     'genero' => 'M', 
+     'area' => 'contabilidade', 
+     'salario' => 4000
+    ],
+     ['nome' => 'Ciclano Pereira', 
+     'genero' => 'M', 
+     'area' => 'engenharia', 
+     'salario' => 4000
+    ],
+     ['nome' => 'Fulana Oliveira', 
+     'genero' => 'F', 
+     'area' => 'contabilidade', 
+     'salario' => 2000
+    ],
+     ['nome' => 'Jão Silveira', 
+     'genero' => 'M', 
+     'area' => 'marketing', 
+     'salario' => 4000
+    ],
+     ['nome' => 'Cirilo Ferreira', 
+     'genero' => 'M', 
+     'area' => 'contabilidade', 
+     'salario' => 3000
+    ],
+     ['nome' => 'Zé das Couve', 
+     'genero' => 'M', 
+     'area' => 'engenharia', 
+     'salario' => 5000
+    ],
+     ['nome' => 'Pedro Pedrada', 
+     'genero' => 'M', 
+     'area' => 'contabilidade', 
+     'salario' => 2000
+    ],
+     ['nome' => 'Paulo Paulada', 
+     'genero' => 'M', 
+     'area' => 'marketing', 
+     'salario' => 2000
+    ],
+     ['nome' => 'Creuza Reis', 
+     'genero' => 'F', 
+     'area' => 'financeiro', 
+     'salario' => 1500
+    ],
+     ['nome' => 'Yarley Ground', 
+     'genero' => 'M', 
+     'area' => 'financeiro', 
+     'salario' => 3000
+    ],
+     ['nome' => 'Billy Bill', 
+     'genero' => 'M', 
+     'area' => 'marketing', 
+     'salario' => 2000
+    ],
+     ['nome' => 'Brian Brioche', 
+     'genero' => 'M', 
+     'area' => 'engenharia', 
+     'salario' => 6500
+    ],
+     ['nome' => 'Mary Mariana', 
+     'genero' => 'F', 
+     'area' => 'financeiro', 
+     'salario' => 2000
+    ],
+ );
+
+
+//Qual o maior salário?
+// $valor = 0;
+// $funcionario = '';
+
+// foreach($funcionarios as $x){
+//     if($x['salario']>$valor){
+//         $valor=$x['salario'];
+//         $funcionario=$x['nome'];
+//     }
+
+// }
+
+// echo "quem recebe o maior salário é ".$funcionario." no valor de ".$valor;
+
+
+
